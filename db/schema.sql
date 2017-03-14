f9022368a3a68ec021dfcc5639572bda767bc961
MeganMFisher <meganmichelle318@gmail.com>
2017-03-14 17:21:24 -0600

    still trying to fix images

--- db/schema.sql
+++ db/schema.sql
@@ -28,22 +28,22 @@ CREATE TABLE products
 
 INSERT INTO products
 (Name, price, description, imageurl) VALUES
- ('Ladies Soft Shell Jacket', 70.00, '100% polyester knit shell bonded to a water-resistant film insert and a 100% polyester mesh interior', '/products/ladiesSoftShellJacket.png'),
- ('End Human Trafficking Tee - Charcoal', 20.00, '60% combed cotton/40% polyester jersey.', '/products/endHumanTraffickingTeeCharcoal.png'),
- ('End Human Trafficking Tee - Navy', 20.00, '60% combed cotton/40% polyester jersey.', '/products/endHumanTraffickingTeeNavy.png'),
- ('Bold and Free Muscle Tank Ladies - Black Slub', 25.00, 'Poly viscose 65/35', '/products/boldandFreeLadiesBlackSlub.png'),
- ('Bold and Free Muscle Tank Ladies - Black Marble', 25.00, 'Poly viscose 65/35', '/products/boldandFreeLadiesBlackMarble.png'),
- ('O.U.R. Full Zip-up Hoodie', 30.00, null, '/products/ourFullZipUpHoodie.png'),
- ('O.U.R. Rescue Men’s Soft Shell Jacket', 70.00, '100% polyester knit shell bonded to a water-resistant film insert and a 100% polyester mesh interior', '/products/ourRescueMensSoftShellJacket.png'),
- ('Men’s Soft Shell Jacket', 70.00, '93/7 poly/spandex woven shell bonded to 100% polyester microfleece with laminated film insert to repel water and wind 1000MM fabric waterproof.', '/products/ourRescueMensJacket.png'),
- ('O.U.R. Rescue Men’s Pullover Jacket', 35.00, '90/10 poly/spandex', '/products/ourRescueMensPulloverJacket.png'),
- ('O.U.R. Rescue Ladies Pullover Jacket', 35.00, '90/10 poly/spandex.', '/products/ourRescueLadiesPulloverJacket.png'),
- ('O.U.R. Hooded Sweatshirt', 35.00, '80% cotton/ 20% polyester.', '/products/ourHoodedSweatshirt.png'),
- ('Freedom for All Tee', 15.00, null, '/products/freedomForAllTee.png'),
- ('I’m an Abolitionist Tee', 12.00, '50/50 Cotton-Poly Blend', '/products/abolitionistTee.png'),
- ('I’m an Abolitionist Ladies Tee', 12.00, '100% Cotton', '/products/abolitionistLadiesTee.png'),
- ('O.U.R. Ladies Tee', 12.00, '100% Cotton', '/products/ourLadiesTee.png'),
- ('O.U.R. Long-sleeve Crew Tee', 12.00, '100% combed cotton jersey.', '/products/ourLongSleeveCrewTee.png');
+ ('Ladies Soft Shell Jacket', 70.00, '100% polyester knit shell bonded to a water-resistant film insert and a 100% polyester mesh interior', './products/ladiesSoftShellJacket.png'),
+ ('End Human Trafficking Tee - Charcoal', 20.00, '60% combed cotton/40% polyester jersey.', './products/endHumanTraffickingTeeCharcoal.png'),
+ ('End Human Trafficking Tee - Navy', 20.00, '60% combed cotton/40% polyester jersey.', './products/endHumanTraffickingTeeNavy.png'),
+ ('Bold and Free Muscle Tank Ladies - Black Slub', 25.00, 'Poly viscose 65/35', './products/boldandFreeLadiesBlackSlub.png'),
+ ('Bold and Free Muscle Tank Ladies - Black Marble', 25.00, 'Poly viscose 65/35', './products/boldandFreeLadiesBlackMarble.png'),
+ ('O.U.R. Full Zip-up Hoodie', 30.00, null, './products/ourFullZipUpHoodie.png'),
+ ('O.U.R. Rescue Men’s Soft Shell Jacket', 70.00, '100% polyester knit shell bonded to a water-resistant film insert and a 100% polyester mesh interior', './products/ourRescueMensSoftShellJacket.png'),
+ ('Men’s Soft Shell Jacket', 70.00, '93/7 poly/spandex woven shell bonded to 100% polyester microfleece with laminated film insert to repel water and wind 1000MM fabric waterproof.', './products/ourRescueMensJacket.png'),
+ ('O.U.R. Rescue Men’s Pullover Jacket', 35.00, '90/10 poly/spandex', './products/ourRescueMensPulloverJacket.png'),
+ ('O.U.R. Rescue Ladies Pullover Jacket', 35.00, '90/10 poly/spandex.', './products/ourRescueLadiesPulloverJacket.png'),
+ ('O.U.R. Hooded Sweatshirt', 35.00, '80% cotton/ 20% polyester.', './products/ourHoodedSweatshirt.png'),
+ ('Freedom for All Tee', 15.00, null, './products/freedomForAllTee.png'),
+ ('I’m an Abolitionist Tee', 12.00, '50/50 Cotton-Poly Blend', './products/abolitionistTee.png'),
+ ('I’m an Abolitionist Ladies Tee', 12.00, '100% Cotton', './products/abolitionistLadiesTee.png'),
+ ('O.U.R. Ladies Tee', 12.00, '100% Cotton', './products/ourLadiesTee.png'),
+ ('O.U.R. Long-sleeve Crew Tee', 12.00, '100% combed cotton jersey.', './products/ourLongSleeveCrewTee.png');
 
 
 CREATE TABLE cart
