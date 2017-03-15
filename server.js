75ad2375fbdfa9545a48b25348467d6e3ff85d9e
MeganMFisher <meganmichelle318@gmail.com>
2017-03-15 15:26:10 -0600

    fix all the things

--- server.js
+++ server.js
@@ -2,47 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var massive = require('massive');
 var cors = require('cors');
-// var morgan = require('morgan');
-// var jwt = require('jwt-simple');
-// var  moment = require('moment');
 
 var app = module.exports = express();
 
-
-// app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '../app/dist'));
-// app.use(morgan('dev'))
 
 var port = 8100;
 
 var config = require('./config.js');
-// var controller = require('./serverCtrl.js'); 
-
-// let ensureAuthenticated = (req, res, next) => { 
-//   if (!req.header('Authorization')) {
-//     return res.status(401).send({
-//       message: 'Please make sure your request has an Authorization header'
-//     });
-//   }
-//   let token = req.header('Authorization').split(' ')[1];
-//   let payload = null;
-//   try {
-//     payload = jwt.decode(token, config.TOKEN_SECRET);
-//   } catch (err) {
-//     return res.status(401).send({
-//       message: err.message
-//     });
-//   }
-//   if (payload.exp <= moment().unix()) {
-//     return res.status(401).send({
-//       message: 'Token has expired'
-//     });
-//   }
-//   req.user = payload.sub;
-//   next();
-// }
-
 
 var db = massive.connect({
     connectionString: config.database
@@ -59,7 +27,10 @@ var db = massive.connect({
     });
   })
 
+
+// var controller = require('./productsCtrl.js');
 var db = app.get('db'); 
+
 
 
 app.get('/products', function(req, res, next){
@@ -67,6 +38,7 @@ app.get('/products', function(req, res, next){
             if(err) {
                 res.send(err);
             } else {
+                //res.json(response)
                 res.status(200).send(response)
             }
         })
@@ -82,59 +54,6 @@ app.get('/products/:id', function(req, res, next){
     })
 });
 
-
-// app.post('/auth/login', controller.login)
-// app.post('/auth/signup', controller.signup)
-// app.get('/api/get-all', controller.getAll)
-
-// app.get('/api/get-all', function(req, res, next) {
-//     db.get_all(function(err, data) {
-//       if (err) return next(err);
-//       else return res.status(200).send(data)
-//     })
-//   })
-
-// app.post('/add-to-cart', controller.addToCart)
-
-// app.post('/add-to-cart', function(req, res, next) {
-//     db.get_order(req.body.userId, function(err, order) {
-//       if (err) return next(err);
-//       if (order[0] && !order[0].completed) {
-//         db.add_to_cart([order[0].id, req.body.item], function(err, cart) {
-//           if (err) return next(err);
-//           db.get_cart(order[0].id, function(err, cart) {
-//             if (err) return next(err);
-//             return res.send(cart);
-//           })
-//         })
-//       } else {
-//         db.create_order(req.body.userId, function(err, data) {
-//           if (err) return next(err);
-//           db.get_order(req.body.userId, function(err, order) {
-//             if (err) return next(err);
-//             db.add_to_cart([order[0].id, req.body.item], function(err, data) {
-//               if (err) return next(err);
-//               db.get_cart(order[0].id, function(err, cart) {
-//                 if (err) return next(err);
-//                 return res.send(cart);
-//               })
-//             })
-//           })
-//         })
-//       }
-//     })
-//   });
-
-// app.post('/check-out', controller.checkOut)
-
-// app.post('/check-out', function(req, res, next) {
-//     db.check_out(req.body.userId, function(err, data) {
-//       if (err) return next(err);
-//       else res.send([]);
-//     })
-//   });
-
-
-app.listen(process.env.PORT || port, function() {
+  app.listen(process.env.PORT || port, function() {
     console.log("Listening on port", this.address().port);
 });
