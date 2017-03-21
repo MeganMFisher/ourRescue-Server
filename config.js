c00213166ae352517ac101b67d6da12e9fbb62d0
MeganMFisher <meganmichelle318@gmail.com>
2017-03-20 18:29:14 -0600

    working with tokens

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports  = {
 
     secret: 'joaipeptuqjprmjfqjapehgpahdnkaflkjd',
-    database: 'postgres://pqgqwtab:ZqknWpdDnQTaf-WBssvNxDX2tfWHXISE@babar.elephantsql.com:5432/pqgqwtab'
+    database: 'postgres://pqgqwtab:ZqknWpdDnQTaf-WBssvNxDX2tfWHXISE@babar.elephantsql.com:5432/pqgqwtab',
+    secretToken: 'apoejfpojaejvodjvpjeopfijaeoijfiajeiojfa'
 
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
-// var session = require('express-session')
 var bodyParser = require('body-parser');
 var massive = require('massive');
 var cors = require('cors');
 var config = require('./config.js');
+// var jwt = require('jsonwebtoken');
+// var cookieParser = require('cookie-parser');
 
 
 var app = module.exports = express();
@@ -11,11 +12,11 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '../app/dist'));
 app.use(cors());
-// app.use(session({
-//   secret: config.secret,
-//   resave: true,
-//   saveUninitialized: true
+
+// app.use(cors({
+//     origin: 'http://192.168.3.104:8100'
 // }));
+// app.use(cookieParser());
 
 var port = 8100;
 
@@ -70,27 +71,46 @@ app.get('/abolitionists', function(req, res, next){
 });
 let fakeSession = [];
 app.post('/api/cart', function(req, res, next){
-
     fakeSession.push(req.body);
-
     res.status(200).send('ok');
 
-
-    // if(Array.isArray(req.session.cart)) {
-    //     req.session.cart.push(req.body)
-    // } else {
-    //     req.session.cart = [req.body]
-    // }
-    // res.status(200).send('ok');
 })
 
 app.get('/api/cart', function(req, res, next){
-
-
     res.status(200).json(fakeSession);
 })
 
+// app.post('/api/cart', function(req, res) {
+//    if(Array.isArray(req.session.cart)) {
+//         req.session.cart.push(req.body)
+//     } else {
+//         req.session.cart = [req.body]
+//     }
+//     res.status(200).send('ok');
+// })
 
+// app.get('/api/cart', function(req, res) {
+//   res.status(200).json(req.session.cart);
+// })
+
+
+// app.post('/api/cart', function(req, res, next){
+//     var token = jwt.sign({cart: 'mycart'}, config.secretToken);
+
+//     res.cookie('my-cookie', token, {maxAge: 100000000, httpOnly: true})
+//     // res.cookie('my-cookie', token, req.body)
+
+//     res.send('ok')
+// })
+
+
+// app.get('/api/cart', function(req, res, next) {
+//     var cart = jwt.verify(req.cookies['my-cookie'], config.secretToken);
+
+//     cart[0] // some product;
+
+//     res.send(cart);
+// })
 
 
   app.listen(process.env.PORT || port, function() {
