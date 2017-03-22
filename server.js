c9c05b526f599b9fb9295288f21b4b4c5aaab1e8
MeganMFisher <meganmichelle318@gmail.com>
2017-03-21 19:33:49 -0600

    cart is working again!

--- server.js
+++ server.js
@@ -16,84 +16,77 @@ var port = 8100;
 
 
 var db = massive.connect({
-    connectionString: config.database
-  },
-    function(err, localdb) {
+        connectionString: config.database
+    },
+    function (err, localdb) {
         db = localdb;
         app.set('db', db);
-        db.schema(function(err, data) {
-      if (err) {
-          console.log(err);
-      } else { 
-          console.log('All tables successfully reset');
-      }
-    });
-  })
-
-
-app.get('/products', function(req, res, next){
-        db.getAllProducts(function(err, response){
-            if(err) {
-                res.send(err);
+        db.schema(function (err, data) {
+            if (err) {
+                console.log(err);
             } else {
-                console.log('success')
-                res.status(200).send(response)
+                console.log('All tables successfully reset');
             }
-        })
-});
+        });
+    })
 
-app.get('/products/:id', function(req, res, next){
-    db.getOneProduct(req.params.id, function(err, response){
-        if(err) {
-            res.status(500).json(err);
+
+app.get('/products', function (req, res, next) {
+    db.getAllProducts(function (err, response) {
+        if (err) {
+            res.send(err);
         } else {
             console.log('success')
-             res.json(response)
+            res.status(200).send(response)
         }
     })
 });
 
-app.get('/abolitionists', function(req, res, next){
-        db.getAbolitionists(function(err, response){
-            if(err) {
-                res.send(err);
-            } else {
-                console.log('success')
-                res.status(200).send(response)
-            }
-        })
+app.get('/products/:id', function (req, res, next) {
+    db.getOneProduct(req.params.id, function (err, response) {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            console.log('success')
+            res.json(response)
+        }
+    })
+});
+
+app.get('/abolitionists', function (req, res, next) {
+    db.getAbolitionists(function (err, response) {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log('success')
+            res.status(200).send(response)
+        }
+    })
 });
 
 
 app.post('/api/cart', (req, res) => {
     var token = req.headers['authorization']
+    var verifiedToken = jwt.verify(token, config.secretToken); // here we are verifying the token
+    let cart = verifiedToken.cart; //there is a cart array on the token 
+    cart.push(req.body) // wer are pushing req.body to the cart array
+    return res.json({
+        token: {token: jwt.sign({cart: cart}, config.secretToken)},
+        cart: cart
+    })
 
-
-    if(req.headers['authorization']){ //I dont know if this is actually testing anything
-        var verifiedToken = jwt.verify(token, config.secretToken); // here we are verifying the token
-        let cart = verifiedToken.cart; //there is a cart array on the token 
-        cart.push(req.body) // wer are pushing req.body to the cart array
-        res.json({token: jwt.sign({cart: cart}, config.secretToken)}) //finally we sign & send a new copy of the cart in a brand new token that will get saved on the front end 
-    } 
-    // else {
-    //     console.log(req.body)
-    //     let myCart = [req.body]
-    //     token = jwt.sign({cart: myCart}, config.secretToken);
-    // }   
-
-
-
-
-    res.json({token: token})
 })
 
 
 app.get('/api/viewcart', (req, res) => {
     var token = req.headers['authorization']
     var verifiedToken = jwt.verify(token, config.secretToken);
-    //cart[0] // some product;
+    var cart = verifiedToken.cart;
 
-    res.send(verifiedToken);
+    return res.json({
+        token: {token: jwt.sign({cart: cart}, config.secretToken)},
+        cart: cart
+    })
 })
 
 app.post('/api/removecart', (req, res) => {
@@ -101,17 +94,38 @@ app.post('/api/removecart', (req, res) => {
     var verifiedToken = jwt.verify(token, config.secretToken);
     var cart = verifiedToken.cart;
     console.log(req.body)
-        for(var i = 0; i < cart.length; i++) {
-            // console.log(cart[i].id, req.body.id)
-            if(cart[i].id === req.body.id) {
-                cart.splice(i, 1);
-                console.log('step2')
-            } 
+    for (var i = 0; i < cart.length; i++) {
+        // console.log(cart[i].id, req.body.id)
+        if (cart[i].id === req.body.id) {
+            cart.splice(i, 1);
+            console.log('step2')
         }
-    res.json({token: jwt.sign({cart: cart}, config.secretToken)})
+    }
+    return res.json({
+        token: {token: jwt.sign({cart: cart}, config.secretToken)},
+        cart: cart
+    })
+})
+
+app.post('/api/update', (req, res) => {
+    var token = req.headers['authorization']
+    var verifiedToken = jwt.verify(token, config.secretToken);
+    let cart = req.body;
+
+    return res.json({
+        token: {token: jwt.sign({cart: cart}, config.secretToken)},
+        cart: cart
+    })
+})
+
+app.post('/api/createToken', (req, res) => {
+     return res.json({
+        token: {token: jwt.sign({cart: cart}, config.secretToken)},
+        cart: []
+    })
 })
 
 
-  app.listen(process.env.PORT || port, function() {
+app.listen(process.env.PORT || port, function () {
     console.log("Listening on port", this.address().port);
 });
