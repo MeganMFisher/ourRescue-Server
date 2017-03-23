94c38b149c9a625033febb9456378162d1a0d7a6
MeganMFisher <meganmichelle318@gmail.com>
2017-03-23 09:32:37 -0600

    stripe working in cart

--- server.js
+++ server.js
@@ -159,45 +159,47 @@ app.post('/api/address', (req, res) => {
   // *             Stripe Endpoints            *
   // *******************************************
 
-//   app.post('/api/payment', (req, res, next) => {
-//   console.log(req.body);
+// payment
+app.post('/api/payment', function(req, res, next){
+  console.log(req.body);
 
-//   const chargeAmt = req.body.amount;
-//   const amountArray = chargeAmt.toString().split('');
-//   const pennies = [];
-//   for (var i = 0; i < amountArray.length; i++) {
-//     if(amountArray[i] === ".") {
-//       if (typeof amountArray[i + 1] === "string") {
-//         pennies.push(amountArray[i + 1]);
-//       } else {
-//         pennies.push("0");
-//       }
-//       if (typeof amountArray[i + 2] === "string") {
-//         pennies.push(amountArray[i + 2]);
-//       } else {
-//         pennies.push("0");
-//       }
-//     	break;
-//     } else {
-//     	pennies.push(amountArray[i])
-//     }
-//   }
-//   const convertedAmt = parseInt(pennies.join(''));
-//   console.log("Pennies: ");
-//   console.log(convertedAmt);
+  const chargeAmt = req.body.amount.price;
+   const amountArray = chargeAmt.toString().split('');
+  const pennies = [];
+  for (var i = 0; i < amountArray.length; i++) {
+    if(amountArray[i] === ".") {
+      if (typeof amountArray[i + 1] === "string") {
+        pennies.push(amountArray[i + 1]);
+      } else {
+        pennies.push("0");
+      }
+      if (typeof amountArray[i + 2] === "string") {
+        pennies.push(amountArray[i + 2]);
+      } else {
+        pennies.push("0");
+      }
+    	break;
+    } else {
+    	pennies.push(amountArray[i])
+    }
+  }
+  const convertedAmt = parseInt(pennies.join(''));
+  console.log("Pennies: ");
+  console.log(convertedAmt);
 
-//   const charge = stripe.charges.create({
-//   amount: convertedAmt, // amount in cents, again
-//   currency: 'usd',
-//   source: req.body.payment.token,
-//   description: 'Test charge from grahms repo'
-// }, function(err, charge) {
-//      res.sendStatus(200);
+
+  const charge = stripe.charges.create({
+  amount: convertedAmt, // amount in cents, again
+  currency: 'usd',
+  source: req.body.payment.token,
+  description: 'Test charge from ionic app'
+}, function(err, charge) {
+     res.sendStatus(200);
 //   if (err && err.type === 'StripeCardError') {
 //     // The card has been declined
 //   }
-// });
-// });
+});
+});
 
 
 app.listen(process.env.PORT || port, function () {
